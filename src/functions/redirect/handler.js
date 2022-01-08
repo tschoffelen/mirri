37a665d4db0c6aefc52a8af0beaf73a79b00ae04
Thomas Schoffelen <thomas@schof.co>
2022-01-08 09:38:47 +0000

    Add /api/redirect endpoint.

---
+++ src/functions/redirect/handler.js
@@ -0,0 +1,40 @@
+import { middleware } from '@flexible-agency/serverless-middleware';
+import { nanoid } from 'nanoid';
+import AWS from 'aws-sdk';
+
+const dependencies = () => ({
+	s3: new AWS.S3({ useAccelerateEndpoint: true })
+});
+
+export const app = async({ body: { url } }, { s3 }) => {
+	if (!url) {
+		throw new Error('Missing required parameters.');
+	}
+
+	const key = nanoid(7);
+
+	await s3.putObject({
+		Bucket: 'schof-link-files',
+		Key: key,
+		ContentType: 'text/html',
+		ContentDisposition: `inline; filename="index.html"`,
+		ACL: 'public-read',
+		Body: `<html><meta http-equiv="refresh" content="0;URL='${url}'"/>Redirecting to ${url}...</html>`,
+		WebsiteRedirectLocation: url
+	}).promise();
+
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			key,
+			url: `https://schof.link/${key}`,
+			publicUrl: `https://schof.link/${key}`,
+		}),
+	};
+};
+
+export const handler = middleware(app).register(dependencies);
